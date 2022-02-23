f11b25a712e1e4224f6596992e5f1241175ed17b
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2022-02-23 16:12:17 -0500

    Added comments to utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,18 @@
+/**
+ * @file Utility functions shared across scripts
+ */
 import fs from "fs";
 import open from "open";
 
+/** The destination folder of all reports - relative to the root */
 const reportDest = "./reports/";
 
+/**
+ * Write the Lighthouse Flow report to disk and open it if desired
+ * @param {String} name The name of the report being generated - results in name.html
+ * @param {String} report The generated Flow report to be written to disk - comes from Flow in the script files
+ * @param {Boolean} openFile Whether or not to open the resulting report file
+ */
 export function writeLighthouseReport(name, report, openFile = true) {
     const output = reportDest + name;
     fs.writeFileSync(output, report);
